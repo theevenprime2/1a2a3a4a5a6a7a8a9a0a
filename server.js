a1eb4a9a220483724df2143360a2806cf63811d4
Jordan Chen <theevenprime2@yahoo.com>
2016-01-30 21:37:31 -0600

    index routes

---
+++ server.js
@@ -0,0 +1,22 @@
+require("./lib/environment")();
+
+var bunyan = require('bunyan');
+
+var log = bunyan.createLogger({
+  name: "server"
+});
+
+var connection = {
+  host: process.env.HOST || "localhost",
+  port: process.env.PORT || "3000"
+}
+
+require('./lib/startup.js')({
+  connection: connection
+}).then(function(server) {
+  log.info('Hapi server started @ ' + server.info.uri);
+}).catch(function(err) {
+  process.nextTick(function() {
+    throw err;
+  })
+});
