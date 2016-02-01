7ac7ac6c0477bfcdf89a74df5737a66e33b8a130
Jordan Chen <theevenprime2@yahoo.com>
2016-01-31 20:07:15 -0600

    basic auth routes; thinky rethinkdb

--- routes/authenticated.js
+++ routes/authenticated.js
@@ -1,13 +1,48 @@
 module.exports = [
   {
-    path: "/auth",
+    path: "/login",
+    method: "POST",
+    config: {
+      auth: 'jwt'
+    },
+    handler: require('./handlers/login')
+  },
+  {
+    path: "/logout",
     method: "GET",
     config: {
       auth: 'jwt'
     },
     handler: function(request, reply) {
-      reply('Welcome!')
-      .header("Authorization", request.headers.authorization);
+      request.auth.session.clear();
+      reply().redirect('/');
+    }
+  },
+  {
+    path: "/register",
+    method: "POST",
+    config: {
+      auth: 'jwt'
+    },
+    validate: {
+      payload: {
+        email: Joi.string().email().required(),
+        password: Joi.string().required()
+      }
+    }
+    handler: function(request, reply) {
+      var newUser = new User({
+        email: request.payload.email
+      });
+
+      User.register(newUser, request.payload.password, function(err, user) {
+        if (err) {
+          return reply(err);
+        }
+
+        console.log(newUser.email + ' has registered!');
+        return reply.redirect('/login');
+      });
     }
   }
 ]
